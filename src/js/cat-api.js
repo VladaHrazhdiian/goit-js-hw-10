8a5acb2e0bb80bf582b4481e8b0943dfb8bd4159
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-07-02 17:30:43 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,23 +1,28 @@
 const API_KEY =
-    'live_97sW46q4IKa50l5Xxxx0Wb1eN7sSjz9AV7OssGjHGgpAAFwwb6o66mTtSuJ0culx';
+  'live_97sW46q4IKa50l5Xxxx0Wb1eN7sSjz9AV7OssGjHGgpAAFwwb6o66mTtSuJ0culx';
 const API_URL = 'https://api.thecatapi.com/v1/';
 const loaderEl = document.querySelector('.loader');
 
-export const fetchBreeds = async () => {
-    loaderEl.style.display = 'block';
-    const res = await fetch(`${API_URL}breeds?api_key=${API_KEY}`);
+export const fetchBreeds = () => {
+  loaderEl.style.display = 'block';
+  return fetch(`${API_URL}breeds?api_key=${API_KEY}`).then(res => {
     if (!res.ok) {
-        throw new error(res.status);
+      throw new Error(res.status);
     }
-    return await res.json();
+
+    return res.json();
+  });
 };
 
-export const fetchCatByBreed = async breedId => {
-    loaderEl.style.display = 'block';
-    const res = await fetch(`${API_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`
-    );
+export const fetchCatByBreed = breedId => {
+  loaderEl.style.display = 'block';
+  return fetch(
+    `${API_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  ).then(res => {
     if (!res.ok) {
-        throw new error(res.status);
+      throw new Error(res.status);
     }
-    return await res.json();
+
+    return res.json();
+  });
 };
